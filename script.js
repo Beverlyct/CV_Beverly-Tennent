1018c8837822785691a584fbb26714915f553d2b
Beverlyct <bctennent@gmail.com>
2025-02-21 15:09:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuToggle = document.querySelector(".menu-toggle");
     const navLinks = document.querySelector(".nav-links");
+    const navOpt = document.querySelectorAll(".nav-links a");
 
     menuToggle.addEventListener("click", () => {
         navLinks.classList.toggle("show");
+    });
+    navOpt.forEach(item => {
+        item.addEventListener("click", () => {
+            navLinks.classList.remove("show");
+        });
+        document.addEventListener("click", (event) => {
+            if (!navLinks.contains(event.target) && !menuToggle.contains(event.target)) {
+                navLinks.classList.remove("show");
+            }
+        });
     });
 });
 const scrollToTopBtn = document.querySelector(".scroll-to-top");
